5d51c6d72f524b32578e0ced1052aecdc922483e
Rizqi Fajri <rizqifajri51@gmail.com>
2024-05-11 00:26:05 +0700

    change background jumbotron

--- src/components/AboutSavvy.jsx
+++ src/components/AboutSavvy.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import about from "../assets/about-savvy.png";
 const AboutSavvy = () => {
   return (
-    <section>
+    <section className="py-24">
       <div className='grid grid-cols-1 sm:grid-cols-2 justify-center items-center lg:mx-56'>
         <img className="object-contain w-60 sm:w-80 lg:w-[600px] mx-auto" src={about} alt='' />
         <div className="flex">
           <span className='hidden sm:flex w-0.5 h-[35vh] bg-gray-200 mx-auto'></span>
-        <div className="flex flex-col mx-5  ">
+        <div className="flex flex-col mx-5 justify-center  ">
           <h1 className='font-bold text-purple-900 text-[30px] lg:text-[40px]'>About Savvy</h1>
           <p className="font-medium text-gray-800 text-[12px] lg:text-[16px] pt-2 text-left">
             Aplikasi manajemen keuangan yang dirancang untuk memberikan Anda
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,7 @@ import React from "react";
 const Button = ({title}) => {
   return (
     <div>
-      <button className='bg-purple-700 w-fit h-fit px-3 py-2 rounded-md text-white'>
+      <button className='bg-purple-700 hover:bg-purple-400 transition-all hover:scale-105 w-fit h-fit px-3 py-2 rounded-md text-white'>
         {title}
       </button>
     </div>
